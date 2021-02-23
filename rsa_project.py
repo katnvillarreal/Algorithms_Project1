52e30f0d8b195f95a02ffa32df1083e77cc0da6d
Kathryn Villarreal <katnvillarreal@gmail.com>
2021-02-23 10:31:05 -0600

    Added in all the encrypting and decrypting function code

--- rsa_project.py
+++ rsa_project.py
@@ -14,12 +14,14 @@ def extended_gcd(a=1, b=1):
     return y, x-a//b*y, d 
 
 def gen_keys():
-    randoms = [rn.randint(10000, 100000) for i in range(100)]
+    randoms = [rn.randint(1000, 10000) for i in range(100)]
     primes = set()
     for i in range(5):
         for j in randoms:
             if pow(rn.randint(2,100), j - 1, j) ==1:
                 primes.add(j)
+
+    primes = list(primes)
     p = primes[0]
     q = primes[1]
     n = p*q
@@ -30,7 +32,7 @@ def gen_keys():
         if math.gcd(primes[i], f) == 1:
             e = primes[i]
             break
-    (x,y,d) = extended_gcd(e, f)
+    (d, x, y) = extended_gcd(e, f)
 
     return p, q, n, f, e, d
 
@@ -38,21 +40,39 @@ def encrypt_message(message, e, n):
     # Given a message string encrypt it using the public key
 
     # Convert string into an array and convert the characters into numbers (Based on ascii)
-    # Raise each of the numbers by e
-    # take each of the numbers and perform % n 
+    # pow(ascii value of character, e, n) 
     # convert back to characters
     # return encrypted message
-    return 0
+    encrypted = list()
+    for i in range(len(message)):
+        if isinstance(message[i], str):
+            new_char = pow((ord(message[i])),e,n)
+        else:
+            new_char = pow(message[i],e,n)
+        try:
+            encrypted.append(chr(new_char))
+        except:
+            encrypted.append(new_char)
+    return encrypted
 
 def decrypt_message(message, d, n):
     # Given an encrypted message decrypt it using the private key
 
     # Convert the string array into an integer array (based on ascii)
-    # Raise each of the numbers to d
-    # take each of the numbers and perform % n 
+    # pow(ascii value of character, d, n) 
     # convert back to characters
     # return the decryted message
-    return 0
+    decrypted = list()
+    for i in range(len(message)):
+        if isinstance(message[i], str):
+            new_char = pow((ord(message[i])),d,n)
+        else:
+            new_char = pow(message[i],d,n)
+        try:
+            decrypted.append(chr(new_char))
+        except:
+            decrypted.append(new_char)
+    return decrypted
 
 def create_signature(signature, d, n):
     # Given a signature, encrypt it using the private key
@@ -60,7 +80,17 @@ def create_signature(signature, d, n):
     # convert string array to integer array
     # (int^d)%n = encrypted signature
     # return encrypted signature
-    return 0
+    encrypted = list()
+    for i in range(len(signature)):
+        if isinstance(signature[i], str):
+            new_char = pow((ord(signature[i])),d,n)
+        else:
+            new_char = pow(signature[i],d,n)
+        try:
+            encrypted.append(chr(new_char))
+        except:
+            encrypted.append(new_char)
+    return encrypted
 
 def decrypt_signature(signature, e, n):
     # Given a signature, decrypt it using the public key
@@ -68,11 +98,39 @@ def decrypt_signature(signature, e, n):
     # convert string array to integer array
     # (int^e)%n = decrypted signature
     # return decryped signature
-    return 0
+    decrypted = list()
+    for i in range(len(signature)):
+        if isinstance(signature[i], str):
+            new_char = pow((ord(signature[i])),e,n)
+        else:
+            new_char = pow(signature[i],e,n)
+        try:
+            decrypted.append(chr(new_char))
+        except:
+            decrypted.append(new_char)
+    return decrypted
 
 if __name__ == "__main__":
     # Create the private and public keys
     (p,q,n,f,e,d) = gen_keys()
+
+    print("P: "+ str(p))
+    print("Q: "+ str(q))
+    print("N: "+ str(n))
+    print("F: "+ str(f))
+    print("E: "+ str(e))
+    print("D: "+ str(d))
+
+    encrypted = encrypt_message("butts", e, n)
+    decrypted = decrypt_message(encrypted, d, n)
+    e_sig = create_signature("Alice", d, n)
+    v_sig = decrypt_signature(e_sig, e, n)
+
+    print("Encryted message: " + str(encrypted))
+    print("Decryted message: " + str(decrypted))
+    print("Encryted signiture: " + str(e_sig))
+    print("Decryted signiture: " + str(v_sig))
+    
 
     # Have the user choose between being a owner or public user
     # If Owner
