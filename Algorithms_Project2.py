fc3972137b590ba0fe45635c86a01d7428b89d82
Kathryn Villarreal <katnvillarreal@gmail.com>
2021-03-06 20:13:20 -0600

    New project BBY

---
+++ Algorithms_Project2.py
@@ -0,0 +1,128 @@
+# Algorithms Project 2
+# Sorting
+# Group Members
+#       Kile Adams
+#       Monique Dashner
+#       Kathryn Villarreal
+
+import time
+import math
+
+# Bubble Sort
+def bubble(numbers):
+    # Best Case (Already Sorted lowest to highest)
+    # Average Case (Not Sorted)
+    # Worst Case (Already Sorted highest to lowest)
+
+    # INSERT ALGORITHM HERE
+
+    # Return sorted array
+    return 0
+
+# Merge Sort
+def merge(numbers):
+    # No matter what case always the same time complexity 
+
+    # INSERT ALGORITHM HERE
+
+    # Return sorted array
+    return 0
+
+# Quick Sort
+def quick(numbers):
+    # Best Case (Always picking the middle number as a pivot)
+    # Average Case 
+    # Worst Case (Always picking the greatest or smallest number as a pivot)
+
+    # INSERT ALGORITHM HERE
+
+    # Return sorted array
+    return 0
+
+# Tree Sort
+def tree(numbers):
+    # Best Case
+    # Average Case
+    # Worst Case
+
+    # INSERT ALGORITHM HERE
+
+    # Return sorted array
+    return 0
+
+def rand_array():
+    # Generate an array with length between 100000 to 1000000
+    # Fill the array with random integers
+
+    # return the generated array
+    return 0
+
+def ascending():
+    # Generate an array with length between 100000 to 1000000
+    # have the array be filled with numbers in ascending order
+
+    # return generated array
+    return 0
+
+def descending():
+    # Generate an array with length between 100000 to 1000000
+    # have the array be filled with numbers in descending order
+
+    # return generated array
+    return 0
+
+def write_file(file, sort, sorted_array, time):
+    # Open the file
+    # Write to the file the sorting algorithm, 
+    #       the sorted array and array length,
+    #       the time it took to sort
+
+    return 0
+
+def average_case(numbers):
+    # Test Bubble sort with random array
+    # Write to file
+    # Test Merge sort with random array
+    # Write to file
+    # Test Quick sort with random array
+    # Write to file
+    # Test Tree sort with random array
+    # Write to file
+
+    return 0
+
+def best_case(numbers):
+    # Test Bubble sort with ascending array
+    # Write to file
+    # Test Merge sort with (doesn't matter) array
+    # Write to file
+    # Test Quick sort with an array that the pivot chosen is always the middle
+    # Write to file
+    # Test Tree sort with (????) array
+    # Write to file
+
+    return 0
+
+def worst_case(numbers):
+    # Test Bubble sort with descending array
+    # Write to file
+    # Test Merge sort with (doesn't matter) array
+    # Write to file
+    # Test Quick sort with an array that the pivot chosen is always the lowest or highest
+    # Write to file
+    # Test Tree sort with (??????) array
+    # Write to file
+
+    return 0
+
+if __name__ == "__main__":
+    print("Oh shit, here we go again")
+    
+    # Generate a random array
+    # Generate ascending array
+    # Generate descending array
+    # Generate any other neccesary arrays
+    
+    # Test best case
+    # Test worst case
+    # test average case
