a31b692eb3a3050be2355cb6c291aef36e377314
Kathryn Villarreal <katnvillarreal@gmail.com>
2021-03-09 19:50:42 -0600

    Added in quick sort and fixed writing to a file

--- Algorithms_Project2.py
+++ Algorithms_Project2.py
@@ -8,6 +8,7 @@
 import time
 import math
 import random as rn
+import statistics as st
 
 # Bubble Sort
 def bubble(numbers):
@@ -15,21 +16,14 @@ def bubble(numbers):
     # Average Case (Not Sorted)
     # Worst Case (Already Sorted highest to lowest)
 
-    # INSERT ALGORITHM HERE
-    n = len(numbers)
-
     # traverse through all array elements
-    for i in range(n-1):
+    for i in range(len(numbers)-1):
     # range(n) also work but outer loop will repeat one more time than needed.
-
-            for j in range(0, n-i-1):
-
-                # traverse the array from 0 to n-i-1
-                # Swap if the element found is greater than the next element
-                if numbers[j] > numbers[j+1]:
-                    numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
-
-
+        for j in range(0, len(numbers)-i-1):
+            # traverse the array from 0 to n-i-1
+            # Swap if the element found is greater than the next element
+            if numbers[j] > numbers[j+1]:
+                numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
     # Return sorted array
     return numbers
 
@@ -40,30 +34,50 @@ def merge(numbers):
     # INSERT ALGORITHM HERE
 
     # Return sorted array
-    return 0
+    emptylist = list()
+    return emptylist
 
-# Quick Sort
-def quick(numbers):
-    # Start at the end and use it as the pivot
-    # Average Case 
-    # Worst Case (Always picking the greatest or smallest number as a pivot)
+# Partition function for quick sort
+def partition(arr, start, end): 
+    i = (start-1)
+    pivot = arr[end]
+    for j in range(start,end): 
+        if arr[j] <= pivot: 
+            i = i+1
+            arr[i], arr[j] = arr[j], arr[i] 
+    arr[i+1], arr[end] = arr[end], arr[i+1] 
+    return (i+1)
 
-    # INSERT ALGORITHM HERE
+def quick(arr, start, end):
+    if len(arr) == 1: 
+        return arr 
+    if start < end: 
+        pi = partition(arr, start,end) 
+        quick(arr, start, pi-1) 
+        quick(arr, pi+1, end) 
+    return arr
 
-    # Return sorted array
-    return 0
+# Partition function for quick sort
+def best_part(arr, start, end): 
+    i = (start-1)
+    pivot = st.median(arr)
+    for j in range(start,end): 
+        if arr[j] <= pivot: 
+            i = i+1
+            arr[i], arr[j] = arr[j], arr[i] 
+    arr[i+1], arr[end] = arr[end], arr[i+1] 
+    return (i+1)
 
-def best_quick(numbers):
-    # Best Case
-    # Always use the median as the pivot
+def best_quick(arr, start, end):
+    if len(arr) == 1: 
+        return arr 
+    if start < end: 
+        pi = best_part(arr, start,end) 
+        best_quick(arr, start, pi-1) 
+        best_quick(arr, pi+1, end) 
+    return arr
 
-    # INSERT ALGORITHM HERE
-
-    # Return sorted array
-    return 0
-
-#   Counting sort function for radix sort
-
+# Counting sort function for radix sort
 def countingSort(numbers, exp):
     n = len(numbers)
 
@@ -94,12 +108,13 @@ def countingSort(numbers, exp):
     # Copying the output array to arr[],
     # so that arr now contains sorted numbers
     i = 0
-    for i in range(0, len(arr)):
+    for i in range(0, len(numbers)):
         numbers[i] = output[i]
 
     return numbers
 
     # Radix Sort
+
 def radix(numbers):
     # Best Case
     # Average Case
@@ -118,7 +133,7 @@ def radix(numbers):
 
 def rand_array():
     # Generate an array with length between 100000 to 1000000
-    length = rn.randint(100000, 1000000)
+    length = rn.randint(100000, 10000000)
     # Fill the array with random integers
     rand_array = [rn.randint(0, 1000000) for i in range(length)]
     # return the generated array
@@ -126,7 +141,7 @@ def rand_array():
 
 def ascending():
     # Generate an array with length between 100000 to 1000000
-    length = rn.randint(100000, 1000000)
+    length = rn.randint(100000, 10000000)
     # have the array be filled with numbers in ascending order
     asc_array = [i for i in range(length)]
     # return generated array
@@ -134,143 +149,126 @@ def ascending():
 
 def descending():
     # Generate an array with length between 100000 to 1000000
-    length = rn.randint(100000, 1000000)
+    length = rn.randint(100000, 10000000)
     # have the array be filled with numbers in descending order
     desc_array = [(length-i) for i in range(length)]
     # return generated array
     return desc_array
 
-def write_file(file, sort, sorted_array, time):
+def write_file(file, sort, sorted, time):
     # Open the file
+    f = open(file, "a")
     # Write to the file the sorting algorithm, 
     #       the sorted array and array length,
     #       the time it took to sort
+    f.write(sort + " Sort Time: " + str(time) + "\n")
+    f.write("Array Length: " + str(len(sorted))+ "\n")
+    f.write("Sorted Array: " + str(sorted) + "\n\n")
 
-    return 0
+    f.close()
 
 def average_case(numbers, file_name):
     f = open(file_name, "a")
-    f.write("Average Case for Algorithms")
+    f.write("Average Case for Algorithms\n")
+    f.close()
 
     srt = time.time()
     # Test Bubble sort with random array
     sorted = bubble(numbers)
     t = time.time()-srt
     # Write to file
-    f.write("Bubble Sort Time: " + str(t))
-    f.write("Array Length: " + str(sorted.len()))
-    f.write("Sorted Array: " + sorted + "\n\n")
+    write_file(file_name, "Bubble", sorted, t)
 
     srt = time.time()
     # Test Merge sort with random array
     sorted = merge(numbers)
     t = time.time()-srt
     # Write to file
-    f.write("Merge Sort Time: " + str(t))
-    f.write("Array Length: " + str(sorted.len()))
-    f.write("Sorted Array: " + sorted + "\n\n")
+    write_file(file_name, "Merge", sorted, t)
 
     srt = time.time()
     # Test Quick sort with random array
-    sorted = quick(numbers)
+    sorted = quick(numbers, 0, (len(numbers)-1))
     t = time.time()-srt
     # Write to file
-    f.write("Quick Sort Time: " + str(t))
-    f.write("Array Length: " + str(sorted.len()))
-    f.write("Sorted Array: " + sorted + "\n\n")
+    write_file(file_name, "Quick", sorted, t)
 
     srt = time.time()
     # Test Radix sort with random array
     sorted = radix(numbers)
     t = time.time()-srt
     # Write to file
-    f.write("Radix Sort Time: " + str(t))
-    f.write("Array Length: " + str(sorted.len()))
-    f.write("Sorted Array: " + sorted + "\n\n")
+    write_file(file_name, "Radix", sorted, t)
 
     return 0
 
 def best_case(numbers, file_name):
     f = open(file_name, "a")
-    f.write("Best Case for Algorithms")
+    f.write("Best Case for Algorithms\n")
+    f.close()
 
     srt = time.time()
     # Test Bubble sort with ascending array
     sorted = bubble(numbers)
     t = time.time()-srt
     # Write to file
-    f.write("Bubble Sort Time: " + str(t))
-    f.write("Array Length: " + str(sorted.len()))
-    f.write("Sorted Array: " + sorted + "\n\n")
+    write_file(file_name, "Bubble", sorted, t)
 
     srt = time.time()
     # Test Merge sort with (doesn't matter) array
     sorted = merge(numbers)
     t = time.time()-srt
     # Write to file
-    f.write("Merge Sort Time: " + str(t))
-    f.write("Array Length: " + str(sorted.len()))
-    f.write("Sorted Array: " + sorted + "\n\n")
+    write_file(file_name, "Merge", sorted, t)
 
     srt = time.time()
     # Test Quick sort with an array that the pivot chosen is always the middle
-    sorted = best_quick(numbers)
+    sorted = best_quick(numbers, 0, len(numbers)-1)
     t = time.time()-srt
     # Write to file
-    f.write("Quick Sort Time: " + str(t))
-    f.write("Array Length: " + str(sorted.len()))
-    f.write("Sorted Array: " + sorted + "\n\n")
+    write_file(file_name, "Quick", sorted, t)
 
     srt = time.time()
-    # Test Tree sort with (????) array
+    # Test Radix sort with (????) array
     sorted = radix(numbers)
     t = time.time()-srt
     # Write to file
-    f.write("Radix Sort Time: " + str(t))
-    f.write("Array Length: " + str(sorted.len()))
-    f.write("Sorted Array: " + sorted + "\n\n")
+    write_file(file_name, "Radix", sorted, t)
 
     return 0
 
 def worst_case(numbers, file_name):
     f = open(file_name, "a")
-    f.write("Worst Case for Algorithms")
+    f.write("Worst Case for Algorithms\n")
+    f.close()
 
     srt = time.time()
     # Test Bubble sort with descending array
     sorted = bubble(numbers)
     t = time.time()-srt
     # Write to file
-    f.write("Bubble Sort Time: " + str(t))
-    f.write("Array Length: " + str(sorted.len()))
-    f.write("Sorted Array: " + sorted + "\n\n")
+    write_file(file_name, "Bubble", sorted, t)
 
     srt = time.time()
     # Test Merge sort with (doesn't matter) array
     sorted = merge(numbers)
     t = time.time()-srt
     # Write to file
-    f.write("Merge Sort Time: " + str(t))
-    f.write("Array Length: " + str(sorted.len()))
-    f.write("Sorted Array: " + sorted + "\n\n")
+    write_file(file_name, "Merge", sorted, t)
 
     srt = time.time()
     # Test Quick sort with an array that the pivot chosen is always the lowest or highest
-    sorted = quick(numbers)
+    sorted = quick(numbers, 0, len(numbers)-1)
     t = time.time()-srt
     # Write to file
-    f.write("Quick Sort Time: " + str(t))
-    f.write("Array Length: " + str(sorted.len()))
-    f.write("Sorted Array: " + sorted + "\n\n")
+    write_file(file_name, "Quick", sorted, t)
 
     srt = time.time()
-    # Test Tree sort with (??????) array
+    # Test Radix sort with (??????) array
     sorted = radix(numbers)
     t = time.time()-srt
     # Write to file
-    f.write("Radix Sort Time: " + str(t))
-    f.write("Array Length: " + str(sorted.len()))
-    f.write("Sorted Array: " + sorted + "\n\n")
+    write_file(file_name, "Radix", sorted, t)
 
     return 0
 
@@ -282,9 +280,10 @@ if __name__ == "__main__":
     # Generate descending array
     descend = descending()
     # Generate any other neccesary arrays
-    
-    print("Oh shit, here we go again")
 
     # Test best case
+    best_case(ascend, "test.txt")
     # Test worst case
+    worst_case(descend, "test.txt")
     # test average case
+    average_case(rand, "test.txt")
