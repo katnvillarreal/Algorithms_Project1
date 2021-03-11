a1d17d434aff33918cff81130bf5e9798aa0e608
kileadams1 <70489595+kileadams1@users.noreply.github.com>
2021-03-11 17:45:03 -0600

    merge sort added

--- Algorithms_Project2.py
+++ Algorithms_Project2.py
@@ -32,7 +32,45 @@ def merge(numbers):
     # No matter what case always the same time complexity 
 
     # INSERT ALGORITHM HERE
+    if len(numbers) > 1:
 
+        # Finding the mid of the array
+        mid = len(numbers) // 2
+
+        # Dividing the array elements
+        L = numbers[:mid]
+
+        # into 2 halves
+        R = numbers[mid:]
+
+        # Sorting the first half
+        merge(L)
+
+        # Sorting the second half
+        merge(R)
+
+        i = j = k = 0
+
+        # Copy data to temp arrays L[] and R[]
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                numbers[k] = L[i]
+                i += 1
+            else:
+                numbers[k] = R[j]
+                j += 1
+            k += 1
+
+        # Checking if any element was left
+        while i < len(L):
+            numbers[k] = L[i]
+            i += 1
+            k += 1
+
+        while j < len(R):
+            numbers[k] = R[j]
+            j += 1
+            k += 1
     # Return sorted array
     emptylist = list()
     return emptylist
